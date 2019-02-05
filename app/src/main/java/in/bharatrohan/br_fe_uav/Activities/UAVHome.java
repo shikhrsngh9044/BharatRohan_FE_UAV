12868dedf86ac64ed5b17899d90e4365923b353d
shikhrsngh9044 <37914855+shikhrsngh9044@users.noreply.github.com>
2019-02-05 16:32:49 +0530

    second commit

---
+++ app/src/main/java/in/bharatrohan/br_fe_uav/Activities/UAVHome.java
@@ -0,0 +1,15 @@
+package in.bharatrohan.br_fe_uav.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.bharatrohan.br_fe_uav.R;
+
+public class UAVHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uavhome);
+    }
+}
